2a9bdcd53a5ecb12598220358a0711cef4f44167
Jerrod Lessel <jerrod.lessel@gmail.com>
2025-06-15 14:16:07 -0700

    Update map.js

--- data/web/map.js
+++ data/web/map.js
@@ -20,6 +20,12 @@ let landslideRaster = null;
 
 proj4.defs("EPSG:3310", "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +datum=NAD83 +units=m +no_defs");
 
+// About Button
+document.getElementById('about-toggle').addEventListener('click', function () {
+  document.getElementById('about-panel').classList.toggle('hidden');
+});
+
+
 // --- Layers ---
 
 // Dynamic Landslide Layer (visual only)
